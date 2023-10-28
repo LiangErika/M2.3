dd5d33b7a9ac62809a3f11e582e02d363f98f159
LiangErika <126982877+LiangErika@users.noreply.github.com>
2023-10-27 22:04:53 -0700

    M2.4
    M2.4 HTTPS

--- server.js
+++ server.js
@@ -5,18 +5,28 @@ const personasRouter = require("./routers/personas");
 const proyectosRouter = require("./routers/proyectos");
 const sequelize = require("./config/databases");
 const app = express();
-//const fs = require("fs");
-//const https = require("https");
+const fs = require("fs");
+const https = require("https");
 
-//process.env.port = 4001
+process.env.port = 4001
+
+const llavePrivada = fs.readFileSync("private.key");
+const certificado = fs.readFileSync("certificate.crt");
+const credenciales = {
+    key: llavePrivada,
+    cert: certificado,
+    passphrase: "desarrolloweb"
+};
 
 app.use(cors());
 app.use(express.json());
 app.use("/persona",personasRouter);
 app.use("/proyecto",proyectosRouter);
 
-sequelize.sync().then(()=> {
-    app.listen(3000,()=>{
-        console.log("server en puerto 3000");
-    })
+const httpsServer = https.createServer(credenciales,app);
+
+httpsServer.listen(process.env.port,()=>{
+    console.log("server en puerto 4001");
+}).on('error',err =>{
+    console.log("Error al iniciarel servidor: ",err);
 })
